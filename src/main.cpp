657b7691a608f4c077d00c209692e1564d422d04
Peter Popov <p.dim.popov@gmail.com>
2022-01-08 15:37:34 +0200

    feat(ServoRotator, logger): implement ServoRotator, implement logger with levels, refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,43 @@
 #include <Arduino.h>
 
+#include <../.pio/libdeps/sparkfun_promicro16/Servo/src/Servo.h> // that include is bad...
+
 // TODO: Separate functionalities into private libraries
 
-//begin DEFINITIONS
-#define LED_PIN 13
-#define BUTTON_PIN 2
-#define BUTTON_HOLD_DIFF_MS 500
-//end DEFINITIONS
+#define bwGet(state, x) (bool)(state & x)
+#define bwSet(state, x, value) (value ? state |= x : state &= ~x)
 
-#define GET(state, x) (bool)(state & x)
-#define SET(state, x, value) (value ? state |= x : state &= ~x)
+struct {
+    static void println(const char level, const char* arg) {
+        Serial.print(level);
+        Serial.print("/");
+        Serial.println(arg);
+    }
+
+    /**
+     * Levels:
+     *  - no logs 0
+     *  - error 1
+     *  - warn 2
+     *  - info 3
+     *  - debug
+     */
+    uint8_t level = 3;
+    bool debugOn = true;
+
+    void info(const char* arg = "") const {
+        if (level >= 3) println('i', arg);
+    }
+    void warn(const char* arg = "") const {
+        if (level >= 2) println('w', arg);
+    }
+    void error(const char* arg = "") const {
+        if (level >= 1) println('e', arg);
+    }
+    void debug(const char* arg = "") const {
+        if (debugOn) println('d', arg);
+    }
+} logger;
 
 struct Led {
     int pin;
@@ -22,6 +50,7 @@ struct Led {
 
 template<typename TContext>
 struct Button {
+    static const uint16_t BUTTON_HOLD_DIFF_MS = 500;
     static const uint8_t IS_HIGH = 0b00000001;
     static const uint8_t IS_BEING_HELD = 0b00000010;
 
@@ -33,9 +62,9 @@ struct Button {
     explicit Button(
             int pin,
             TContext& context,
-            void(*onClick)(const TContext&) = nullptr,
-            void(*onHold)(const TContext&) = nullptr,
-            void(*onRelease)(const TContext&) = nullptr
+            void(*onClick)(TContext&) = nullptr,
+            void(*onHold)(TContext&) = nullptr,
+            void(*onRelease)(TContext&) = nullptr
         ):
         m_pin(pin),
         m_context(context),
@@ -47,50 +76,46 @@ struct Button {
     }
 
     void listen() {
-        auto prevIsHigh = GET(state, IS_HIGH);
-        auto isHigh = GET(SET(state, IS_HIGH, digitalRead(m_pin)), IS_HIGH);
+        auto prevIsHigh = bwGet(state, IS_HIGH);
+        auto isHigh = bwGet(bwSet(state, IS_HIGH, digitalRead(m_pin)), IS_HIGH);
 
-        if (!prevIsHigh && isHigh) {
-            onDown();
-        } else if (prevIsHigh && isHigh && !GET(state, IS_BEING_HELD) && (millis() - downStartTime > BUTTON_HOLD_DIFF_MS)) {
-            onStartHolding();
-        } else if (prevIsHigh && !isHigh) {
-            onUp();
-        }
+        if (!prevIsHigh && isHigh) onDown();
+        else if (prevIsHigh && isHigh && !bwGet(state, IS_BEING_HELD) && (millis() - downStartTime > BUTTON_HOLD_DIFF_MS)) onStartHolding();
+        else if (prevIsHigh && !isHigh) onUp();
     }
 
 private:
 #pragma region handlers
-    void(*m_onClick)(const TContext&);
+    void(*m_onClick)(TContext&);
     void onClick() {
         if (m_onClick) m_onClick(m_context);
     }
 
-    void(*m_onHold)(const TContext&);
+    void(*m_onHold)(TContext&);
     void onHold() {
         if (m_onHold) m_onHold(m_context);
     }
 
-    void(*m_onRelease)(const TContext&);
+    void(*m_onRelease)(TContext&);
     void onRelease() {
         if (m_onRelease) m_onRelease(m_context);
     }
 #pragma endregion handlers
 
     void onStartHolding() {
-        SET(state, IS_BEING_HELD, true);
+        bwSet(state, IS_BEING_HELD, true);
         onHold();
     }
 
     void onDown() {
         downStartTime = millis();
-        Serial.println("button down");
+        logger.info("button down");
     }
 
     void onUp() {
-        Serial.println("button up");
-        SET(state, IS_HIGH, false);
-        SET(state, IS_BEING_HELD, false);
+        logger.info("button up");
+        bwSet(state, IS_HIGH, false);
+        bwSet(state, IS_BEING_HELD, false);
 
         auto releaseTime = millis();
         if (releaseTime - downStartTime <= BUTTON_HOLD_DIFF_MS) onClick();
@@ -99,31 +124,80 @@ private:
     }
 };
 
+struct ServoRotator {
+    static const uint16_t DEFAULT_OPEN_TIME_MS = 1000;
+    static const uint8_t OPENED_DEGREES = 180;
+    static const uint8_t CLOSED_DEGREES = 0;
+
+    static const uint8_t IS_OPEN = 0b00000001;
+    static const uint8_t IS_TIMED = 0b00000010;
+
+    int m_pin = 0;
+
+    uint32_t closeTime = 0;
+
+    Servo servo;
+    uint8_t state = 0b00000000;
+
+    explicit ServoRotator(const int pin): m_pin{pin} {
+        servo.attach(pin);
+        servo.write(CLOSED_DEGREES);
+    }
+
+    void open() {
+        servo.write(OPENED_DEGREES);
+        bwSet(state, IS_OPEN, true);
+    }
+
+    template<typename TTimePeriodMs = uint16_t>
+    void openTimed(TTimePeriodMs timePeriodMs = DEFAULT_OPEN_TIME_MS) {
+        open();
+        bwSet(state, IS_TIMED, true);
+        closeTime = millis() + timePeriodMs;
+    }
+
+    void close() {
+        logger.info("closing");
+        bwSet(state, IS_OPEN, false);
+        bwSet(state, IS_TIMED, false);
+        servo.write(CLOSED_DEGREES);
+    }
+
+    void listen() {
+        if (bwGet(state, IS_OPEN) && bwGet(state, IS_TIMED) && millis() >= closeTime) close();
+    }
+};
+
 struct Program {
-    Led led13 = Led(LED_PIN);
-    Button<Program> button2 = Button<Program>(
-        BUTTON_PIN,
-        *this,
-        [](const Program &program) {
-            Serial.println("clicked");
-            program.led13.toggle();
-        },
-        [](const Program& program){
-            Serial.println("held");
-            program.led13.turnOn();
-        },
-        [](const Program& program){
-            Serial.println("released");
-            program.led13.turnOff();
-        }
-    );
+    Led redLed = Led{13};
+    ServoRotator servoRotator{9};
+    Button<Program> rotatorButton{
+            2,
+            *this,
+            [](Program &program) {
+                logger.debug("clicked");
+                program.redLed.toggle();
+                program.servoRotator.openTimed();
+            },
+            [](Program &program) {
+                logger.debug("held");
+                program.redLed.turnOn();
+                program.servoRotator.open();
+            },
+            [](Program &program) {
+                logger.debug("released");
+                program.redLed.turnOff();
+                program.servoRotator.close();
+            }
+    };
 
     Program() {
         Serial.begin(9600);
     }
 
     void run() {
-        button2.listen();
+        rotatorButton.listen();
+        servoRotator.listen();
     }
 };
 
